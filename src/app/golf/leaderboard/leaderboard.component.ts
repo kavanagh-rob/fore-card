dea29c46934d9a3d21880e9501edff362c077ee6
Rob <kavanagh.m.rob@gmail.com>
2018-04-28 16:39:56 +0100

    added styling to scores birdies etc

--- src/app/golf/leaderboard/leaderboard.component.ts
+++ src/app/golf/leaderboard/leaderboard.component.ts
@@ -31,6 +31,36 @@ export class LeaderboardComponent implements OnInit {
   getLastUpdatedHole(scorecard) {
     return scorecard.baseScores.length === 0 ? 'n/a' : scorecard.baseScores.length;
   }
+  getScoreClass(scorecard, index) {
+    const strokeScore = parseInt(this.getStrokeScore(scorecard, index), 10);
+    const par = parseInt(this.round.course.holes[index].par, 10);
+    let resolvedClass = '';
+    if (strokeScore) {
+      switch (par - strokeScore) {
+        case -1:
+          resolvedClass = 'bogie';
+          break;
+        case 0:
+          resolvedClass = 'par';
+          break;
+        case 1:
+          resolvedClass = 'birdie';
+          break;
+        case 2:
+          resolvedClass = 'eagle';
+          break;
+        case 3:
+          resolvedClass = 'albatross';
+          break;
+        default:
+          resolvedClass = '';
+        }
+    }
+    return resolvedClass;
+  }
+  getStrokeScore(scorecard, index) {
+    return scorecard.baseScores[index] ? scorecard.baseScores[index] : '';
+  }
   getStablefordScore(scorecard, index) {
     return scorecard.stablefordScores[index] ? scorecard.stablefordScores[index] : '';
   }
